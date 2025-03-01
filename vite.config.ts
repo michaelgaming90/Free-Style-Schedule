854112bb750ff74abaf974343dbeb79baf331a52
michaelgaming90 <alicpalamichael90@gmail.comm>
2025-03-01 23:18:07 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,12 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/pwa-icon-192.png",
+            src: "/Free-Style-Schedule/pwa-icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-icon-512.png",
+            src: "/Free-Style-Schedule/pwa-icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
